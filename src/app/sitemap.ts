7b91566ea09357c63983aee9cfde54386de6d004
Mike Olaski <mike.olaski@mikeolaski.com>
2026-03-19 20:19:42 +0100

    Add /sprint page as homepage duplicate

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,18 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    return [
-        {
-            url: "https://audit.rehabit.ai",
-            lastModified: new Date(),
-            changeFrequency: "weekly",
-            priority: 1,
-        },
-    ];
+  return [
+    {
+      url: "https://audit.rehabit.ai",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://audit.rehabit.ai/sprint",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+  ];
 }
